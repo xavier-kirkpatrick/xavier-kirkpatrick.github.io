c90bcc3d3806c565f22239665842100561fdb1c7
Xavier Kirkpatrick <kirkpatrickxavier@gmail.com>
2024-05-07 09:48:56 +1200

    commit for push

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -4,8 +4,8 @@ function App() {
   const { data } = useFruits()
 
   return (
-    <div className="app">
-      <h1>Fullstack Boilerplate - with Fruits!</h1>
+    <div className="">
+      <h1>Personal Portfolio</h1>
       {/* <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul> */}
       <ul>{data?.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
     </div>
