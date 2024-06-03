2d25e9797d425d4e53b4825d5c5057fb9c1589a6
Xavier Kirkpatrick <kirkpatrickxavier@gmail.com>
2024-06-04 11:00:00 +1200

    configure Million-Lint + framer-motion

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,17 @@
+import { motion } from "framer-motion";
+
 function Header() {
   return (
-    <div className=" w-80% ml-56 border-b border-black pt-10 font-raleway text-2xl">
-      <h1>Xavier</h1>
-      <h1>Kirkpatrick</h1>
+    <div>
+      <motion.div
+        className=" w-80% ml-56 border-b border-black pt-10 font-raleway text-2xl"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <h1>Xavier</h1>
+        <h1>Kirkpatrick</h1>
+      </motion.div>
     </div>
   );
 }
