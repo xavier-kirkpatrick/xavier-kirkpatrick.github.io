27db8984e1f146966c75c06c74a322dd39048747
Xavier Kirkpatrick <kirkpatrickxavier@gmail.com>
2024-08-13 14:03:14 +1200

    reformat text on right side of header component

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -1,8 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchWeatherData } from "../apis/weatherApi";
+import { motion } from "framer-motion";
 import Marquee from "react-fast-marquee";
 
 function DisplayWeather() {
+  // Delays the render of the weather marquee by 2 seconds.
+  const delayWeather = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 2, delay: 2 } },
+  };
   // Fetch weather + asto data, calls new data at 8 minute intervals.
   const { data, isLoading, error } = useQuery({
     queryKey: ["weather"],
@@ -21,7 +27,7 @@ function DisplayWeather() {
 
   // Display weather + asto data on front end and handel loading + error states.
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div></div>;
   }
   if (error) {
     return <div>Error: {(error as ErrorType).message}</div>;
@@ -34,17 +40,22 @@ function DisplayWeather() {
       pauseOnHover={true}
       delay={4}
       gradient={true}
-      gradientWidth={25}
+      gradientWidth={15}
       gradientColor="#f1f1ec"
     >
-      <div className="flex flex-row space-x-6 text-[15px] tracking-wide">
+      <motion.div
+        variants={delayWeather}
+        initial="hidden"
+        animate="visible"
+        className="flex flex-row space-x-4 text-[15px] tracking-wide"
+      >
         <p>Weather: {data?.weather.current.condition.text}</p>
         <p>Temp: {data?.weather.current.temp_c}&deg;C</p>
-        <p>Pressure: {data?.weather.current.pressure_mb}mb</p>
+        <p>Pressure: {data?.weather.current.pressure_mb} mb</p>
         <p>UV Index: {data?.weather.current.uv}</p>
         <p>Sunset: {data?.astronomy.astronomy.astro.sunset}</p>
         <p></p>
-      </div>
+      </motion.div>
     </Marquee>
   );
 }
