721703b426bebb6f5ddf37fcac71ebf65e9f142a
Xavier Kirkpatrick <kirkpatrickxavier@gmail.com>
2024-08-27 16:10:07 +1200

    add sparkle pulse effect to move me hints

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,100 +1,75 @@
-import { useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import { fadeInText } from "../motion_variants/fadeInText";
-import Marquee from "react-fast-marquee";
+import { ParentDragContext } from "./__root";
+import { useContext } from "react";
 
-// The Index component is rendered inside the root route or __route.tsx via the outlet.
-// This is done via the createLazyFileRoute function which in configured to render Index below.
+// The Homw component is rendered inside the root route or __route.tsx via the outlet.
+// This is done via the createFileRoute function which in configured to render dynamically below.
 
 export const Route = createFileRoute("/")({
   component: Home,
 });
 
 function Home() {
-  const [currentMarquee, setCurrentMarquee] = useState("left");
+  const parentRef = useContext(ParentDragContext);
 
-  const leftDrag = {
-    top: -50,
-    left: -50,
-    right: 150,
-    bottom: 50,
-  };
-
-  const rightDrag = {
-    top: -50,
-    left: -150,
-    right: 50,
-    bottom: 50,
+  const sparkleEffect: Variants = {
+    effect: {
+      opacity: [1, 0.5, 1, 0.4, 0.6, 1, 0.4, 1, 0.7],
+      transition: { duration: 4, repeat: Infinity, repeatType: "loop" },
+    },
   };
 
   return (
+    // Container div
     <motion.div
-      className="flex min-h-full flex-wrap items-center justify-center space-x-0 space-y-0 text-contentFontSize md:space-x-6"
+      className="flex min-h-full flex-wrap items-center justify-center space-x-0 space-y-0 text-contentFontSize md:space-x-6 md:space-y-0"
       variants={fadeInText}
       initial="initial"
       animate="animate"
     >
+      {/* Left text box */}
       <motion.div
         drag
-        dragConstraints={leftDrag}
+        dragConstraints={parentRef ?? undefined}
         className="w-[350px] hover:text-blueLink"
       >
+        <motion.div
+          variants={sparkleEffect}
+          animate="effect"
+          className="text-center text-xs italic text-blueLink"
+        >
+          Move me
+        </motion.div>
         <p>
           Welcome, I’m a Software Developer based out of Tāmaki Makaurau,
           Auckland, New Zealand.
         </p>
-        <div>
-          {currentMarquee === "left" && (
-            <Marquee
-              autoFill={false}
-              loop={1}
-              speed={35}
-              direction="right"
-              delay={0}
-              gradient={true}
-              gradientWidth={5}
-              gradientColor="#f1f1ec"
-              onFinish={() => setCurrentMarquee("right")}
-              className="mb-1 mr-4 mt-1 inline-block text-xs italic text-blueLink"
-            >
-              Move me!!
-            </Marquee>
-          )}
-        </div>
 
         <p>
           It is an exciting time for me as I pivot into the tech industry, and
           in 2024, I am looking to progress my career in Development.
         </p>
       </motion.div>
+
+      {/* Right text box */}
       <motion.div
         drag
-        dragConstraints={rightDrag}
+        dragConstraints={parentRef}
         className="w-[350px] hover:text-blueLink"
       >
+        <motion.div
+          variants={sparkleEffect}
+          animate="effect"
+          className="text-center text-xs italic text-blueLink"
+        >
+          Move me
+        </motion.div>
         <p>
           Here you can find work completed during my studies as well as other
           external projects.
         </p>
-        <div>
-          {currentMarquee === "right" && (
-            <Marquee
-              autoFill={false}
-              loop={1}
-              speed={35}
-              direction="left"
-              delay={0}
-              gradient={true}
-              gradientWidth={5}
-              gradientColor="#f1f1ec"
-              onFinish={() => setCurrentMarquee("left")}
-              className="mb-1 mt-1 inline-block text-xs italic text-blueLink"
-            >
-              Move me!!
-            </Marquee>
-          )}
-        </div>
 
         <p>
           You’ll also find other non-development related interests where I spend
