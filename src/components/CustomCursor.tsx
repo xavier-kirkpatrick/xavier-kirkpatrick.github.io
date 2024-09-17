8fd029f744b7716b719eb1bb7e3d13d99620b82f
Xavier Kirkpatrick <kirkpatrickxavier@gmail.com>
2024-09-17 14:09:26 +1200

    add cursor change to + sign and hide defult cursor

---
+++ src/components/CustomCursor.tsx
@@ -0,0 +1,28 @@
+import { useRef, useEffect } from "react";
+import "../styles/custom_cursor.css";
+// import throttle from "lodash/throttle";
+
+function CustomCursor() {
+  const cursorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const trackCursorMovements = (event: MouseEvent) => {
+      if (cursorRef.current) {
+        cursorRef.current.style.left = `${event.clientX}px`;
+        cursorRef.current.style.top = `${event.clientY}px`;
+      }
+    };
+    window.addEventListener("mousemove", trackCursorMovements);
+
+    return () => {
+      window.removeEventListener("mousemove", trackCursorMovements);
+    };
+  }, []);
+  return (
+    <div className="custom-cursor" ref={cursorRef}>
+      +
+    </div>
+  );
+}
+
+export default CustomCursor;
