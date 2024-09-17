cf93f7e40f7f591f5e6e44e7de4c637bd56b8646
Xavier Kirkpatrick <kirkpatrickxavier@gmail.com>
2024-09-17 14:50:33 +1200

    reconfig millionlint

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
+import MillionLint from '@million/lint';
 import MillionCompiler from "@million/lint";
 import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-
+const _plugins = [MillionCompiler.vite(), react(), TanStackRouterVite()];
+_plugins.unshift(MillionLint.vite())
 export default defineConfig({
-  plugins: [MillionCompiler.vite(), react(), TanStackRouterVite()],
+  plugins: _plugins,
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
-    },
-  },
+      "/api": "http://localhost:3000"
+    }
+  }
   // base: "xavier-kirkpatrick.github.io",
 });
